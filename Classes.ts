f9fc51d326b34572ee8e8dec806f3638756e0be4
PabloCabreraR <pablocabrera2g@gmail.com>
2021-06-04 15:08:32 +0200

    Learning typescript

---
+++ Classes.ts
@@ -0,0 +1,11 @@
+class Car{
+    brand:string
+    date:string
+    doors:number
+
+    constructor(brand_:string, date_:string, doors_:number){
+        this.brand = brand_
+        this.date = date_
+        this.doors = doors_
+    }
+}
