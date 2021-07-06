3671735c8bfe385e8678b59705bc86b09538e2ef
PabloCabreraR <pablocabrera2g@gmail.com>
2021-07-06 15:39:22 +0200

    void

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === "as-number") {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
+    // if (resultConversion === "as-number"){
+    //     return +result
+    // } else {
+    //     return result.toString()
+    // }
 }
-var combinesAges = combine(30, 27);
+var combinesAges = combine(30, 27, "as-number");
 console.log(combinesAges);
-var combinesNames = combine('Pablo', "Ana");
+var combinesNames = combine('Pablo', "Ana", "as-text");
+console.log(combinesNames);
+var combinesStringAges = combine('30', "27", "as-number");
+console.log(combinesStringAges);
--- app.ts
+++ app.ts
@@ -1,14 +1,12 @@
-function combine(input1:number | string,input2:number | string) {
-    let result
-    if (typeof input1 === 'number' && typeof input2 === 'number'){
-        result = input1 + input2
-    } else {
-        result = input1.toString() + input2.toString()
-    }
-    return result
+function add(n1: number, n2: number){
+    return n1 + n2
 }
 
-const combinesAges = combine(30, 27)
-console.log(combinesAges)
-
-const combinesNames = combine('Pablo', "Ana")
+function printResult(num: number): void { // Void means the function doesnt return anything
+    console.log("Result " + num)
+}
+// function printResult(num: number): undefined {
+//     console.log("Result " + num)
+//     return
+// }
+printResult(add(5,12))
