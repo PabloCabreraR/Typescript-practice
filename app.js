6f1ab9e2f2ea35b1aa5cb645773c3d44370eb477
PabloCabreraR <pablocabrera2g@gmail.com>
2021-07-06 18:06:58 +0200

    function type for callbacks

--- app.js
+++ app.js
@@ -8,4 +8,17 @@ function printResult(num) {
 //     console.log("Result " + num)
 //     return
 // }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+addAndHandle(8, 4, function (result) {
+    console.log(result);
+});
 printResult(add(5, 12));
+// let combinesValues: Function  // Any function.
+var combinesValues; // Declaring combineValues will only store a function with 2 number arguments and returns a number.
+combinesValues = add;
+// combinesValues = printResult
+// combinesValues = 5
+console.log(combinesValues(8, 8));
