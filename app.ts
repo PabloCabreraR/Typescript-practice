a4b6d3821fc149061dad7c6ceab0f29b1ec417f4
PabloCabreraR <pablocabrera2g@gmail.com>
2021-07-06 15:12:20 +0200

    enum & any

--- app.ts
+++ app.ts
@@ -7,16 +7,25 @@
 // }
 
 
-const person: {
-    name: string
-    age: number
-    hobbies: string[]
-    role: [number, string] // Tuple, fixed length and fixed types.
-} = {
+// const person: {
+//     name: string
+//     age: number
+//     hobbies: string[]
+//     role: [number, string] // Tuple, fixed length and fixed types.
+// } = {
+//     name: "Pablo",
+//     age: 27,
+//     hobbies: ["Sports", "Cookies"],
+//     role: [2, 'author']
+// }
+
+enum Role {ADMIN = 2, READ_ONLY, AUTHOR}
+
+const person = {
     name: "Pablo",
     age: 27,
     hobbies: ["Sports", "Cookies"],
-    role: [2, 'author']
+    role: Role.ADMIN
 }
 
 console.log(person.name)
